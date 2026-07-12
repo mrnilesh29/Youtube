9d99faa31f490b74f62df58b5e1a417b39c58c72
Nilesh Shah <mr.nilesh29@gmail.com>
2026-07-12 16:19:54 +0530

    Strings and type casting

---
+++ Phase 1/day 3/main.py
@@ -0,0 +1,63 @@
+# #type conversion and type casting 
+# #explicit type conversion 
+# #implecit type conversion 
+
+
+# age = 10/3   # 
+# print(age)
+
+# A = 10/10 
+# print(A)
+
+# A = float("4.5")
+# print(type(A))
+
+# b = str(3453)
+# print(type(b))
+
+# s = "Nilesh"
+# print(ord("A"))
+# print(ord("B"))
+# print(ord("Z"))
+# print(ord("a"))
+# print(chr(128522))
+
+# print(chr(3476))
+
+
+# s = "CodeXNilesh"
+# print(s[-11])
+# print(s[10])
+
+# code = "CodeXNilesh_____"
+# print(s[5])
+
+
+# print(s)
+
+# print(s[5:11:2]) #  [s:s:s]   n+1 
+
+# x = bool("asd")
+# print(x)
+
+
+# print("hello world")
+# print("hdlkfghsdkj")
+
+# age = input("Enter your age => ")
+# print(age)
+
+# name = input("Enter your name => ")
+# print(f"thank you sir my name is {name}")
+
+# rupess = int(input("Enter you rupees => "))
+# print(rupess)
+# print(type(rupess))
+
+
+# a = 100 
+# b = 20 
+# print(a%b)
+
+print(6**4)   # 6*6*6
+
