4a015b8aa355e50ef11b6eab123b8d5c5d4a522f
Nilesh Shah <mr.nilesh29@gmail.com>
2026-07-12 17:49:52 +0530

    All Operators

---
+++ Phase 1/day 4/main.py
@@ -0,0 +1,71 @@
+# # a = 11 
+# # b = 2 
+
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+
+# # print(a/b)
+# # print(a//b)  
+
+# # print(a%b)
+
+# # print(10**4) #2 * 2* 2 
+
+
+# a = 30
+# name = "akarsh"
+
+
+# print(a)
+# print(a+5) 
+
+# # a = a +5 
+# # a += 5 
+
+# # a = a-5 
+# # a -= 5 
+
+
+
+# print(a)
+
+# a = 100
+# b = 100
+# print(a != b)
+
+# print(a<=b)
+
+
+# print("Nilesh" == "Nileshh")
+
+
+#logical operators 
+
+# r = ("Nilesh" == "Nilesh") and ("akarsh"== "akarsh")
+# #true and true
+# print(r)
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+
+
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+
+b = not False
+print(b)
+
+
+
+
+
+
+
+    
