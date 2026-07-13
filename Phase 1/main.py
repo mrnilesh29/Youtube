b1ae7af50f2e4cdb05098835c46c74aff7dd7be6
Nilesh Shah <mr.nilesh29@gmail.com>
2026-07-13 12:41:25 +0530

    for Loop

---
+++ Phase 1/main.py
@@ -0,0 +1,27 @@
+# print(range(1,101,1))
+# #starting #stop  #step 
+
+# # for i in range(1,6,1):
+# #     print(i)
+    
+    
+# s = "Nature"
+# print(len(s))
+
+# for i in range(0,len(s),1):
+#     print(s[i])
+    
+    
+# for i in s : 
+#     print(i)
+    
+for i in range(1,21,1): 
+
+    if i == 20 : 
+        break
+    print(i)
+else : 
+    print("else")
+
+    
+
