81007765413407c4a667c249e4daabfe8341a0c0
Nilesh Shah <mr.nilesh29@gmail.com>
2026-07-19 10:49:08 +0530

    Tuple Dictonary sets

---
+++ Phase 1/day 11/exception_handeling.py
@@ -0,0 +1,34 @@
+# print("Hello World")
+
+# age = 10 
+# if age >18 :
+#     print("Vote")
+    
+
+# a = int(input("enter a number = "))
+
+# try : 
+#     print(10/a)
+# except ZeroDivisionError as err : 
+#     print(err)
+# else : 
+#     print("This is else Block")
+# finally : 
+#     print("this is finally block ")
+
+
+# print("End")
+
+
+# age = int(input("Enter your age = "))
+# try:
+#     if age > 30 and age < 5 : 
+#         print("Welcome to study hub")
+#     else : 
+#         raise "your age must be 5 to 30 " 
+# except Exception as err : 
+#     print(err)
+    
+# print("End")
+
+
---
+++ Phase 1/day 11/file_handeling.py
@@ -0,0 +1,10 @@
+# r = open(r'marvel.txt','r')
+# print(r.read())
+
+# w = open(r"ironman.txt" , 'w') 
+# # w.write("iron man is the great superhero")
+# w.write("iron man 3 was a good movies")
+
+
+# a = open(r'ironman.txt','a')
+# a.write("iron man is the tony stark ")
