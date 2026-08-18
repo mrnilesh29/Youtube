b3c2354dcec971fff144163ac27b46e337263684
Nilesh Shah <mr.nilesh29@gmail.com>
2026-08-18 09:25:34 +0530

    sliding_window

---
+++ Python DSA/Arrays/Pattern 4 — Sliding Window/max_odd_count.py
@@ -0,0 +1,20 @@
+def odd_count(arr, k):
+    # your code
+    count = 0 
+    maxx = 0 
+    
+    for i in range(k) : 
+        if arr[i]%2 !=0  : count +=1 
+        
+    maxx = max(count,maxx)
+    i = 0 
+    for j in  range(k,len(arr)): 
+        if arr[i] %2 !=0 : count -=1 
+        if arr[j] %2  !=0 : count += 1 
+        
+        maxx = max(maxx,count)
+        i+=1 
+    return maxx
+        
+    
+print(odd_count([1,3,5,2,4],2))
